ce3be63b812a9b626027dd8ea0dbd56f96689156
Iván A. <hello@alleksy.com>
2023-10-25 15:17:21 -0700

    changed text for buttons

--- buttonsfunctions.py
+++ buttonsfunctions.py
@@ -7,16 +7,5 @@ def buttonone():
     return(test1)
     
 def buttontwo():
-    try:
-        response = requests.get('https://toncenter.com/api/v2/getAddressBalance?address=EQDnRzHz7T4X8zjso6CV0tuHNf42GrxCdlU78ijc6d6Y6AbO')
-        response.raise_for_status()
-        # access JSON content
-        jsonResponse = response.json()
-        print("Entire JSON response")
-        print(jsonResponse)
-    except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred: {err}')
-
-    print(jsonResponse["result"])
+    test2 = "this is btn1"
+    return(test2)
